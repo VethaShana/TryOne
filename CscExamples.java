22ced5d6b4ece222781b2753f086dd9aba65d5b6
VethaShana <vvvttt479@gmail.com>
2020-09-18 21:09:22 +0530

    Add

--- CscExamples.java
+++ CscExamples.java
@@ -2,5 +2,27 @@ class CscExamples{
 	int bookNo;
 	String BookName;
 	String category;
-	
+
+	public void setBookNo(int b){
+		bookNo = b;
+	}
+	public void setBookName(String a){
+		BookName = a;
+	}
+	public void setCategory(String c){
+		category = c;
+	}
+
+	public int getBookNo(){
+		return bookNo;
+	}
+
+	public String getBookName(){
+		return BookName;
+	}
+
+	public String getCategory(){
+		return category;
+	}
+
 }
