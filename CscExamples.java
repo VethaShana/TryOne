04e3bafdd90a5b2a984ffd59daf2bec02c06fb7b
VethaShana <vvvttt479@gmail.com>
2020-09-18 21:17:45 +0530

    added

--- CscExamples.java
+++ CscExamples.java
@@ -2,5 +2,14 @@ class CscExamples{
 	int bookNo;
 	String BookName;
 	String category;
+
+	public void BookCollection(int a)
+	{
+		bookNo= a+1;
+	}
+
+	public void BookCategory(String t){
+		category=t;
+	}
 	
 }
